3192883cd673dbb6c8e66a7408fb4fb2fadf63a3
Pricelesstv <104768848+Pricelesstv@users.noreply.github.com>
2022-11-08 22:56:33 +0100

    Add files via upload

---
+++ AfleveringFredag/BilInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfleveringFredag
+{
+    internal  class BilInfo
+    {
+        public string BilMærke { get; set; }
+        public string BilModel { get; set; }
+        public string BilÅrgang { get; set; }
+        public DateTime BilSyn { get; set; }
+        public DateTime SynDato { get; set; } = DateTime.Today;
+        public string synshal()
+        {
+            return SynDato.ToString("D");
+        }
+    }
+}
+
+.
---
+++ AfleveringFredag/Gui.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AfleveringFredag
+{
+    internal class Gui
+    {
+        int år = 730;
+        public Gui()
+        {
+            BilInfo bilInfo = new BilInfo();
+            bilInfo.BilMærke = GetString("Bilens Mærke: ");
+            bilInfo.BilModel = GetString("Bilens Model: ");
+            bilInfo.BilÅrgang = GetString("Bilens Årgang: ");
+            bilInfo.BilSyn = synshal();
+
+            var Today = DateTime.Today;
+
+            if (bilInfo.BilSyn>Today.AddYears(-2))
+            {
+                Console.WriteLine("´Din bil skal ikke til syn");
+
+            }
+            else 
+            {
+
+                Console.WriteLine("Din Bil skal til syn");
+            }
+
+
+
+        }
+        private string GetString(string type)
+        {
+            string input;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(type);
+                input = Console.ReadLine();
+            }
+            while (input == null && input != "");
+            return input;
+        }
+        private DateTime synshal()
+        {
+            DateTime dateOnly;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Bilens sidst synet dato: ");
+            } while (!DateTime.TryParse(Console.ReadLine(), out dateOnly));
+            return dateOnly;
+        }
+    }
+}
